76c34e44d3f1a2857f0acfefe964fd9106cfcc15
CesarFGuerra <cesarfrancaguerra@gmail.com>
2022-08-13 10:04:50 -0300

    Projeto criado

---
+++ src/test/java/com/cesarfranca/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarfranca.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
