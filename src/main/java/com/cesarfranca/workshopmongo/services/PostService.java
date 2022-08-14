ac1bb8b3d1735882fa783d8c32f6dd0849bc7ba3
CesarFGuerra <cesarfrancaguerra@gmail.com>
2022-08-13 22:08:14 -0300

    Consulta com varios critérios

--- src/main/java/com/cesarfranca/workshopmongo/services/PostService.java
+++ src/main/java/com/cesarfranca/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.cesarfranca.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullsearch(String text, Date minDate, Date maxDate){
+		//formula feita para acrescentar um dia a data inserida
+		maxDate = new Date(maxDate.getTime() + 8460*1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
